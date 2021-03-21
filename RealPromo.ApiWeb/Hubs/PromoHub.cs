201934393711de77504e7c8c7abc2e43c917a9d1
rodrigolima <rodrigo.lima131@gmail.com>
2021-03-21 12:30:16 -0300

    Adicionar arquivos de projeto.

---
+++ RealPromo.ApiWeb/Hubs/PromoHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealPromo.ApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealPromo.ApiWeb.Hubs
+{
+    
+    //RPC
+    public class PromoHub : Hub
+    {
+        /**
+         * Cliente - JS/C#/Java
+         * RPC
+         * Cliente(Javascript) - > Hub(C#)  (C# - CastrarPromocao)
+         * Hub (C#)-> Cliente (ReceberPromocao)
+         * */
+
+
+        public async Task CadastrarPromocao(Promocao promocao)
+        {
+            /*
+             * Banco
+             * Queue/Scheduler
+             * */
+
+            await Clients.Caller.SendAsync("CadastradoSucesso"); //Notificar Caller - > Cadastrado realizado com sucesso.
+            await Clients.Others.SendAsync("ReceberPromocao",promocao); //Notificar Promoção Chegou.
+        }
+
+    }
+}
---
+++ RealPromo/RealPromo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using RealPromo.Models;
+using RealPromo.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RealPromo
+{
+    public partial class MainPage : ContentPage
+    {
+
+        ObservableCollection<Promocao> lista = new ObservableCollection<Promocao>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //GetPromocoes();
+            ListViewPromocao.ItemsSource = lista;
+
+            new RealPromoSignalR(lista);
+
+
+            /**
+            Device.StartTimer(TimeSpan.FromSeconds(10),() => {
+                lista.Add(new Promocao { Empresa = "Carrefour", Chamada = "TV em promoções" +DateTime.Now.ToString(), Regras = "10 unidades", EnderecoURL = "http://www.carrefour.com.br" });
+                return true;
+            });**/
+        }
+        /**
+        private void GetPromocoes()
+        {
+            lista.Add(new Promocao{Empresa="Carrefour",Chamada = "TV em promoções", Regras = "10 unidades",EnderecoURL = "http://www.carrefour.com.br"});
+            lista.Add(new Promocao{Empresa="Carrefour",Chamada = "Notebooks", Regras = "20 unidades",EnderecoURL = "http://www.carrefour.com.br"});
+        }**/
+    }
+}
